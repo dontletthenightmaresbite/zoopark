84a3582a374542b2c083b19730977933a65d3f5a
dontletthenightmaresbite <118385440+dontletthenightmaresbite@users.noreply.github.com>
2022-11-22 16:56:29 +0300

    Update penguin.py

--- animals/penguin.py
+++ animals/penguin.py
@@ -20,3 +20,18 @@ class Penguin:
 
     def play(self):
         print(self.name + " играет")
+    """
+    @property
+    def isFeeded(self):
+        return self.__foodAte >= self.__amountOfFood
+
+    def eatFood(self, food, amount):
+        if not self.isFeeded:
+            if food in self.__food:
+                self.__foodAte += amount
+                print(self.__name + " съел" + (" " if self.__gender else "а ") + self.__foods[food])
+            else:
+                print(self.__vid, "это не ест")
+        else:
+            print(self.__name, "наел" + ("ся" if self.__gender else "ась"))
+        #print(self.__name + " съел" + (" " if self.gender else "а ") + str(self.amountOfFood) + " кг " + self.food[randint(0,len(self.food))-1])"""
